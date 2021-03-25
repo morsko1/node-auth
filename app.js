b2217580e738783f066de257e2b82b259403f9ad
morsko1 <morssko1@gmail.com>
2021-03-25 11:19:09 +0300

    added auth

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const routes = require('./routes');
 const errorHandler = require('./handlers/error-handler');
+require('./handlers/auth');
+
+mongoose.connect(
+  'mongodb://127.0.0.1:27017/auth',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+mongoose.set('useCreateIndex', true);
+mongoose.connection.on('error', error => console.log(error) );
+mongoose.Promise = global.Promise;
+
 
 const app = express();
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,55 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
-router.post('/register', function(req, res, next) {
-  res.send({success: true});
+router.post(
+  '/register',
+  passport.authenticate('register', { session: false }),
+   (req, res, next) => {
+    res.json({
+      message: 'Signup successful',
+      user: req.user
+    });
 });
 
-router.get('/login', function(req, res, next) {
-  res.send({success: true});
-});
+router.post(
+  '/login',
+  async (req, res, next) => {
+    passport.authenticate(
+      'login',
+      async (err, user, info) => {
+        try {
+          if (err || !user) {
+            const error = new Error('An error occurred.');
+
+            return next(error);
+          }
+
+          req.login(
+            user,
+            { session: false },
+            async (error) => {
+              if (error) return next(error);
+
+              const body = { _id: user._id, email: user.email };
+              const token = jwt.sign({ user: body }, 'TOP_SECRET');
+
+              // set cookie
+              res.cookie('token', token, {
+                expires: new Date(Date.now() + 604800000),
+                secure: false, // set to true if your using https
+                httpOnly: true,
+              });
+              return res.json({ token });
+            }
+          );
+        } catch (error) {
+          return next(error);
+        }
+      }
+    )(req, res, next);
+  }
+);
 
 module.exports = router;
