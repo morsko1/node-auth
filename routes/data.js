31ffed93f7bb755d650feef6855eed13a642bce0
morsko1 <morssko1@gmail.com>
2021-03-25 11:36:48 +0300

    secured data route

--- routes/data.js
+++ routes/data.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
-router.get('/data', function(req, res, next) {
-  res.send({data: 'data'});
-});
+router.get(
+  '/data',
+  passport.authenticate('jwt', { session: false }),
+  (req, res, next) => {
+    res.send({data: 'data'});
+  }
+);
 
 module.exports = router;
